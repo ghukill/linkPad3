18cfa3e89ba32770b3ad068d73addb9ee4ba23f9
root <root@mitten.(none)>
2014-11-29 23:28:20 +0000

    first positive responses

--- linkPad3/views.py
+++ linkPad3/views.py
@@ -1,5 +1,15 @@
-# fm2
+# linkPad3
 from linkPad3 import app
+from linkPad3 import models
+
+# get celery instance / handle
+from cl.cl import celery
+
+# Solr
+from solrHandles import solr_handle
+
+# localConfig
+import localConfig
 
 # python modules
 import time
@@ -13,17 +23,13 @@ import shlex, subprocess
 import socket
 import hashlib
 import os
+from datetime import datetime
 
 # flask proper
 from flask import render_template, request, session, redirect, make_response, Response, Blueprint
-from flask.ext.sqlalchemy import SQLAlchemy
-from datetime import datetime
 
-# get celery instance / handle
-from cl.cl import celery
 
-# Solr
-from solrHandles import solr_handle
+
 
 # session data secret key
 ####################################
@@ -32,11 +38,23 @@ app.secret_key = 'linkPad3'
 
 
 
-@app.route("/")
-def index():
+@app.route("/<current_page>")
+def index(current_page):
 
-	return "<p>Boom!</p>"
+	'''
+	Create some kind of search class in models.py that accepts the pagination object
+	'''
 
+	solr_params = {
+		"q":"python",
+		"start":0,
+		"rows":localConfig.per_page
+	}
+	search_results = solr_handle.search(**solr_params)
+
+	pagination = models.Pagination(current_page, localConfig.per_page, search_results.total_results)
+
+	return render_template("index.html",pagination=pagination,search_results=search_results)
 
 
 
@@ -48,7 +66,7 @@ def index():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):    
-	return "<p>Cannot find that, dummy.  For your helath!"
+	return "<p>Cannot find that, dummy.  For your health!"
 
 
 
